0439712db71dd02d2704fb912babd0c1b3322c1e
Kovalenko <matvik7@mail.ru>
2018-04-07 03:04:55 +0300

    aaabbb

--- src/by/it/Kovalenko/lesson02/TaskC1.java
+++ src/by/it/Kovalenko/lesson02/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.Kovalenko.lesson02;
 
+import org.hamcrest.core.SubstringMatcher;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,7 +12,16 @@ package by.it.Kovalenko.lesson02;
 Sum = 26
 */
 class TaskC1 {
+    public static void main(String[] args) {
 
+        int number1 = 5;
+        int number2 = 5;
+        int k = number1 + number2;
+
+        System.out.println(number1 + "+" + number2 + "=" + k);
+
+
+    }
 
 
 
