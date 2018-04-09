e67248c788b62d3f66f1a7130b636e2191022925
Inessa <matvik7@mail.ru>
2018-04-09 23:24:10 +0300

    a + a2 + b1 + b2

--- src/by/it/Kovalenko/lesson03/TaskB2.java
+++ src/by/it/Kovalenko/lesson03/TaskB2.java
@@ -1,5 +1,7 @@
 package by.it.Kovalenko.lesson03;
 
+import java.util.Scanner;
+
 /*
 Lesson 03. Task B2. Нужно написать программу, которая вводит три числа  a b c типа int с клавиатуры
 и вычисляет корни квадратного уравнения a*a*x + b*x + c = 0:
@@ -31,4 +33,32 @@ Lesson 03. Task B2. Нужно написать программу, котора
 */
 class TaskB2 {
 
+    public static void main(String[] args) {
+        double a, b, c;
+        double D;
+        System.out.println("Программа решает квадратное уравнение вида:");
+        System.out.println("ax^2 + bx + c = 0");
+        System.out.println("Введите a, b и c:");
+        Scanner in = new Scanner(System.in);
+        a = in.nextDouble();
+        b = in.nextDouble();
+        c = in.nextDouble();
+        D = b * b - 4 * a * c;
+        if (D > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(D)) / (2 * a);
+            x2 = (-b + Math.sqrt(D)) / (2 * a);
+            System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = " + x2);
+        }
+        else if (D == 0) {
+            double x;
+            x = -b / (2 * a);
+            System.out.println("Уравнение имеет единственный корень: x = " + x);
+        }
+        else {
+            System.out.println("Уравнение не имеет действительных корней!");
+        }
+
+    }
 }
+
