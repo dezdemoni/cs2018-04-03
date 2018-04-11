7f8f71ca78ba5c308946a4718a3160b356976154
Inessa <matvik7@mail.ru>
2018-04-12 00:34:01 +0300

    a + a2 + a3 + b1+b2(??)

--- src/by/it/Kovalenko/lesson04/TaskB2.java
+++ src/by/it/Kovalenko/lesson04/TaskB2.java
@@ -24,5 +24,20 @@ size прочитать c клавиатуры. Числа разделить п
 3. Программа должна выводить таблицу умножения size х size.
  */
 public class TaskB2 {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 1;
+        while (i != 11) ;
+        {
+            while (j != 11) ;
+            {
+                System.out.println(i * j + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+            j = 1;
 
+        }
+    }
 }
