b61e4171a940d82e9c3b3aacf6304aa4d008fa3b
Kovalenko <matvik7@mail.ru>
2018-04-12 21:46:49 +0300

    a + a2 5

--- src/by/it/Kovalenko/lesson04/TaskB2.java
+++ src/by/it/Kovalenko/lesson04/TaskB2.java
@@ -25,19 +25,12 @@ size прочитать c клавиатуры. Числа разделить п
  */
 public class TaskB2 {
     public static void main(String[] args) {
-        int i = 1;
-        int j = 1;
-        while (i != 11) ;
-        {
-            while (j != 11) ;
-            {
-                System.out.println(i * j + " ");
-                j++;
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + " ");
             }
             System.out.println();
-            i++;
-            j = 1;
-
         }
+
     }
 }
